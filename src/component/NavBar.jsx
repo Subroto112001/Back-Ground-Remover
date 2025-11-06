52d0545feb9804cccff60b507e8420a8398bc03a
Subroto112001 <subrotokumarbarman@gmail.com>
2025-11-06 16:17:52 +0600

    Buycredit page and google login

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
-
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const NavBar = () => {
-  
+  const { openSignIn } = useClerk();
+
+  const { isSignedIn, user } = useUser();
+
   return (
     <div className="flex justify-between items-center mx-4 py-3 lg:mx-44">
       <div>
@@ -18,12 +20,22 @@ const NavBar = () => {
           />
         </Link>
       </div>
-      <button className="flex flex-row justify-center items-center px-4 sm:px-8 py-2 sm:py-3 gap-4 rounded-full bg-zinc-800 text-white text-sm font-medium cursor-pointer hover:bg-[#454545] transition duration-300">
-        Get Started{" "}
-        <span>
-          <FaArrowRight />
-        </span>
-      </button>
+
+      {isSignedIn ? (
+        <div>
+          <UserButton/>
+        </div>
+      ) : (
+        <button
+          onClick={() => openSignIn({})}
+          className="flex flex-row justify-center items-center px-4 sm:px-8 py-2 sm:py-3 gap-4 rounded-full bg-zinc-800 text-white text-sm font-medium cursor-pointer hover:bg-[#454545] transition duration-300"
+        >
+          Get Started{" "}
+          <span>
+            <FaArrowRight />
+          </span>
+        </button>
+      )}
     </div>
   );
 };
